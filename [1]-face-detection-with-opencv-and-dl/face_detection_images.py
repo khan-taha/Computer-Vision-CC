c0bdc21a013bcd9f74c08338c75c058687931147
khan-taha <49602667+khan-taha@users.noreply.github.com>
2020-11-05 22:48:32 +0500

    Update face_detection_images.py

--- [1]-face-detection-with-opencv-and-dl/face_detection_images.py
+++ [1]-face-detection-with-opencv-and-dl/face_detection_images.py
@@ -42,22 +42,23 @@ net.setInput(blob)
 
 detections = net.forward()
 print("detections : " + detections)
+
 # loop over the detections
+
 for i in range(0, detections.shape[2]):
     # extract the confidence (i.e., probability) associated with the prediction
-	confidence = detections[0, 0, i, 2]
+    confidence = detections[0, 0, i, 2]
 	# filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
-	if confidence > args["confidence"]:
-        # compute the (x, y)-coordinates of the bounding box for the object
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        print("box : " + box)
+    if confidence > args["confidence"]:
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
-        print("(startX, startY, endX, endY) : " + (startX, startY, endX, endY))
-		# draw the bounding box of the face along with the associated probability
-		text = "{:.2f}%".format(confidence * 100)
-		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-		cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        # draw the bounding box of the face along with the associated probability
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(image, (startX, startY), (endX, endY),(0, 0, 255), 2)
+        cv2.putText(image, text, (startX, y),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        
 # show the output image
 cv2.imshow("Output", image)
 cv2.waitKey(0)
