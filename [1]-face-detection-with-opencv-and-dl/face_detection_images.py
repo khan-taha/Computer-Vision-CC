858812d4ad17346425d763f6949a896e85089d8a
khan-taha <49602667+khan-taha@users.noreply.github.com>
2020-11-05 22:37:09 +0500

    Update face_detection_images.py

--- [1]-face-detection-with-opencv-and-dl/face_detection_images.py
+++ [1]-face-detection-with-opencv-and-dl/face_detection_images.py
@@ -56,10 +56,8 @@ for i in range(0, detections.shape[2]):
 		# draw the bounding box of the face along with the associated probability
 		text = "{:.2f}%".format(confidence * 100)
 		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.rectangle(image, (startX, startY), (endX, endY),
-			(0, 0, 255), 2)
-		cv2.putText(image, text, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+		cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 # show the output image
 cv2.imshow("Output", image)
 cv2.waitKey(0)
